0a43348ecbf55709b3f09b60a3161753e54a56b1
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-05-11 14:28:24 +0300

    order python server-client

--- year 2/תקשורת ומחשוב/קודים/server-client/client.py
+++ year 2/תקשורת ומחשוב/קודים/server-client/client.py
@@ -1,18 +1,14 @@
 import socket
 
- 
+msgFromClient = "Hello UDP Server"
+bytesToSend = str.encode(msgFromClient)
 
-msgFromClient       = "Hello UDP Server"
-bytesToSend         = str.encode(msgFromClient)
-
-serverAddressPort   = ("127.0.0.1", 20001)
-bufferSize          = 1024
-
+serverAddressPort = ("127.0.0.1", 20001)
+bufferSize = 1024
 
 # Create a UDP socket at client side
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
- 
 # Send to server using created UDP socket
 UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
--- year 2/תקשורת ומחשוב/קודים/server-client/server.py
+++ year 2/תקשורת ומחשוב/קודים/server-client/server.py
@@ -1,14 +1,11 @@
 import socket
 
-localIP     = "127.0.0.1"
-localPort   = 20001
-bufferSize  = 1024
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
 
- 
-msgFromServer       = "Hello UDP Client"
-bytesToSend         = str.encode(msgFromServer)
-
- 
+msgFromServer = "Hello UDP Client"
+bytesToSend = str.encode(msgFromServer)
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -21,13 +18,13 @@ print("UDP server up and listening")
  
 # Listen for incoming datagrams
 
-while(True):
+while True:
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
 
     clientMsg = "Message from Client:{}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
+    clientIP = "Client IP Address:{}".format(address)
     
     print(clientMsg)
     print(clientIP)
