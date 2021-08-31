6bb45e5080602ec61b65383b92143a19af141067
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-08-31 11:56:07 +0300

    add Parse XPath to Java code

---
+++ year 2/Databases/קודים/xml/XPathParse.java
@@ -0,0 +1,27 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.*;
+import java.io.File;
+import java.io.IOException;
+
+public class XPathParse {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException {
+        File inputFile = new File("src/xml/student.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document xmlDoc = builder.parse(inputFile);
+
+        XPathFactory xPathfactory = XPathFactory.newInstance();
+        XPath xpath = xPathfactory.newXPath();
+        XPathExpression expr = xpath.compile("University/Student[2]/Address/City");
+
+        String city = (String)expr.evaluate(xmlDoc, XPathConstants.STRING);
+        System.out.println("City: " + city);
+    }
+}
