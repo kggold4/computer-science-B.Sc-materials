562d1fb4a5874c8e1c5cb9f317713b75b41354a5
kggold4 <68167734+kggold4@users.noreply.github.com>
2021-08-31 10:02:54 +0300

    add Parse XML to Java code

---
+++ year 2/Databases/קודים/xml/XMLParse.java
@@ -0,0 +1,114 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+class Student {
+    public String firstName;
+    public String lastName;
+    public int id;
+    public int age;
+    public Address address;
+    public String toString() {
+        return "ID: " + id + "\nName: " + firstName + " " + lastName + "\nAge: " + age + "\nAddress: " + address + "\n";
+    }
+}
+
+class Address {
+    public String street;
+    public String city;
+    public String zip;
+    public String toString() {
+        return street + " " + city + " " + zip;
+    }
+}
+
+public class XMLParse {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        // Students list
+        List<Student> studentList = new ArrayList<>();
+
+        // get xml file
+        File inputFile = new File("src/xml/student.xml");
+
+        // create factory
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+        // get builder from factory
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        // create document from xml file (parse with builder)
+        Document doc = builder.parse(inputFile);
+
+        // print root name
+        System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+        // getting all the students node into a list
+        NodeList nodeList = doc.getDocumentElement().getElementsByTagName("Student");
+        for(int studentIdx = 0; studentIdx < nodeList.getLength(); studentIdx++) {
+            Node studentNode = nodeList.item(studentIdx);
+            if(studentNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) studentNode;
+                Student student = new Student();
+                studentList.add(student);
+
+                // print attribute degree
+                System.out.println("Degree: " + element.getAttribute("degree"));
+                NodeList studentAllNode = studentNode.getChildNodes();
+                for(int stIdx = 0; stIdx < studentAllNode.getLength(); stIdx++) {
+                    Node stInnerNode = studentAllNode.item(stIdx);
+                    switch(stInnerNode.getNodeName()) {
+                        case "FirstName":
+                            student.firstName = stInnerNode.getTextContent();
+                            break;
+                        case "LastName":
+                            student.lastName = stInnerNode.getTextContent();
+                            break;
+                        case "id":
+                            student.id = Integer.parseInt(stInnerNode.getTextContent());
+                            break;
+                        case "age":
+                            student.age = Integer.parseInt(stInnerNode.getTextContent());
+                            break;
+                        case "Address":
+                            Address address = new Address();
+                            student.address = address;
+                            NodeList addressAllNodes = stInnerNode.getChildNodes();
+                            for(int adIdx = 0; adIdx < addressAllNodes.getLength(); adIdx++) {
+                                Node adInnerNode = addressAllNodes.item(adIdx);
+                                switch(adInnerNode.getNodeName()) {
+                                    case "Street":
+                                        address.street = adInnerNode.getTextContent();
+                                        break;
+                                    case "City":
+                                        address.city = adInnerNode.getTextContent();
+                                        break;
+                                    case "Zip":
+                                        address.zip = adInnerNode.getTextContent();
+                                        break;
+                                }
+                                student.address = address;
+                            }
+                    }
+                }
+            }
+        }
+
+        System.out.println();
+        for(Student st : studentList) {
+            System.out.println(st);
+        }
+    }
+}
