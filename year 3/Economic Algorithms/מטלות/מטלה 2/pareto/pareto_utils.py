7f340713eaddea99fc7bac1f02014265c48d6e68
Kfir Goldfarb <kggold4@gmail.com>
2022-11-06 21:38:50 +0200

    add test to pareto optimal algorithm

--- year 3/Economic Algorithms/מטלות/מטלה 2/pareto/pareto_utils.py
+++ year 3/Economic Algorithms/מטלות/מטלה 2/pareto/pareto_utils.py
@@ -1,38 +1,81 @@
 from typing import List
 from agent import Agent
-import numpy as np
 
 
 def isParetoImprovement(agents: List[Agent], option1: int, option2: int) -> bool:
     """
-    Check each agent in the given agents list: return true if the first option value is less than the second
+    Check each agent in the given agents list: return true if the first option value is less than the second,
     else: return false
+
+    ...
+
+    Tests
+    -----
+
+    Option 1 is Pareto Improvement of option 2?
+    >>> isParetoImprovement(test_agents, 0, 1)
+    False
+
+    Option 4 is Pareto Improvement of option 5?
+    >>> isParetoImprovement(test_agents, 3, 4)
+    False
+
+    Option 3 is Pareto Improvement of option 2?
+    >>> isParetoImprovement(test_agents, 2, 1)
+    True
+
     """
-    answer = True
-    for agent_index in range(len(agents)):
-        if agents[agent_index].value(option1) < agents[agent_index].value(option2):
-            answer = False
-            break
-    return answer
+    for agent in agents:
+        if agent.value(option1) < agent.value(option2):
+            return False
+    return True
 
 
 def isParetoOptimal(agents: List[Agent], option: int, allOptions: List[int]) -> bool:
-    # temp matrix that receive values of 0 and 1 and with its help
-    # we will check whether the current option is Pareto Optimal
-    check_matrix = np.ones((len(agents), len(allOptions)))
-    for i in range(len(allOptions)):
-        for j in range(len(agents)):
-            if i != option:
-                if agents[j].value(option) <= agents[j].value(allOptions[i]):
-                    check_matrix[j][i] = 0
+    """
+    For each option in allOptions that different from the given option check if is a pareto improvement,
+    If it is, return false, else: return true
 
-    # Go over the whole temp matrix: if there is one column that is all with zeros - that means this option is Pareto Optimal,
-    # Otherwise - if there is no column full of zeros - necessarily this option is Pareto Optimal
-    for j in range(len(check_matrix[0])):
-        for i in range(len(check_matrix)):
-            if j != option:
-                if check_matrix[i][j] == 1:
-                    break
-                if i == len(check_matrix) - 1 and check_matrix[i][j] == 0:
-                    return False
+    ...
+
+    Tests
+    -----
+
+    Option 1 is Pareto Optimal?
+    >>> isParetoOptimal(test_agents, 0, test_options)
+    True
+
+    Option 2 is Pareto Optimal?
+    >>> isParetoOptimal(test_agents, 1, test_options)
+    False
+
+    Option 3 is Pareto Optimal?
+    >>> isParetoOptimal(test_agents, 2, test_options)
+    True
+
+    Option 4 is Pareto Optimal?
+    >>> isParetoOptimal(test_agents, 3, test_options)
+    True
+
+    Option 5 is Pareto Optimal?
+    >>> isParetoOptimal(test_agents, 4, test_options)
+    True
+
+    """
+    for opt in allOptions:
+        if opt != option and isParetoImprovement(agents, opt, option):
+            return False
     return True
+
+
+if __name__ == '__main__':
+    # example
+    agent_1 = Agent([1, 2, 3, 4, 5])
+    agent_2 = Agent([3, 1, 2, 5, 4])
+    agent_3 = Agent([3, 5, 5, 1, 1])
+    test_agents = [agent_1, agent_2, agent_3]
+    test_options = [0, 1, 2, 3, 4]
+    import doctest
+
+    (failures, tests) = doctest.testmod(report=True)
+    print(f"{failures} failures, {tests} tests")
