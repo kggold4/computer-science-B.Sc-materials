b0dcd690e1f83a790ec9818b47529f43d6c99967
kggold4 <kggold4@gmail.com>
2023-01-09 23:07:21 +0200

    order code

--- year 3/Economic Algorithms/Assignments/assignment_10/main.py
+++ year 3/Economic Algorithms/Assignments/assignment_10/main.py
@@ -1,4 +1,5 @@
 import statistics
+from typing import List
 
 
 def compute_budget(total_budget: float, citizen_votes: list[list]) -> list[float]:
@@ -35,35 +36,32 @@ def compute_budget(total_budget: float, citizen_votes: list[list]) -> list[float
     return binary_search(low=0, high=1, citizen_votes=citizen_votes, total_budget=total_budget)
 
 
-def binary_search(low, high, citizen_votes: list[list], total_budget: float):
+def binary_search(low: int, high: int, citizen_votes: List[list], total_budget: float) -> List[int]:
     if high >= low:
-        mid = (high + low) / 2
-        medians = []
+        middle: int = (high + low) / 2
+        medians: List[int] = []
         for citizen_vote in range(len(citizen_votes[0])):
-            arr_median = []
+            medians_array = []
             current_medians = []
             for citizen_vote_index in range(1, len(citizen_votes)):
-                current_medians.append(total_budget * min(1, citizen_vote_index * mid))
-            arr_median += current_medians
-            inner_medians = []
+                min_citizen_index = min(citizen_vote_index * middle, 1)
+                current_medians.append(total_budget * min_citizen_index)
+            medians_array += current_medians
+            current_vote_values = []
             for current_vote in citizen_votes:
-                inner_medians.append(current_vote[citizen_vote])
-            arr_median += inner_medians
-            medians.append(statistics.median(arr_median))
-
-        sum_medians = sum(medians)
+                current_vote_values.append(current_vote[citizen_vote])
+            medians_array += current_vote_values
+            medians.append(statistics.median(medians_array))
 
         # Recursively binary search
-        if sum_medians > total_budget:
-            return binary_search(low=low, high=mid, citizen_votes=citizen_votes, total_budget=total_budget)
-        elif sum_medians < total_budget:
-            return binary_search(low=mid, high=high, citizen_votes=citizen_votes, total_budget=total_budget)
-        else:
-            return medians
-    return -1
+        if sum(medians) > total_budget:
+            return binary_search(low=low, high=middle, citizen_votes=citizen_votes, total_budget=total_budget)
+        elif sum(medians) < total_budget:
+            return binary_search(low=middle, high=high, citizen_votes=citizen_votes, total_budget=total_budget)
+        return medians
 
 
-def check_fairness(total_budget: float, citizen_votes: list[list[float]], budget: list[float]) -> bool:
+def check_fairness(total_budget: float, citizen_votes: List[List[float]], budget: List[float]) -> bool:
     """
     Question 5 b
 
@@ -72,19 +70,19 @@ def check_fairness(total_budget: float, citizen_votes: list[list[float]], budget
     >>> check_fairness(100, citizen_votes, budget)
     True
 
-    >>> citizen_votes2 = [[50, 50, 0], [0, 50, 50]]
-    >>> budget2 = compute_budget(100, citizen_votes2)
-    >>> check_fairness(100, citizen_votes2, budget2)
+    >>> citizen_votes = [[50, 50, 0], [0, 50, 50]]
+    >>> budget = compute_budget(100, citizen_votes)
+    >>> check_fairness(100, citizen_votes, budget)
     False
 
-    >>> citizen_votes2 = [[7, 8], [9, 7]]
-    >>> budget2 = compute_budget(16, citizen_votes2)
-    >>> check_fairness(16, citizen_votes2, budget2)
+    >>> citizen_votes = [[7, 8], [9, 7]]
+    >>> budget = compute_budget(16, citizen_votes)
+    >>> check_fairness(16, citizen_votes, budget)
     False
 
-    >>> citizen_votes2 = [[3, 0, 0, 3], [0, 3, 3, 0]]
-    >>> budget2 = compute_budget(6, citizen_votes2)
-    >>> check_fairness(6, citizen_votes2, budget2)
+    >>> citizen_votes = [[3, 0, 0, 3], [0, 3, 3, 0]]
+    >>> budget = compute_budget(6, citizen_votes)
+    >>> check_fairness(6, citizen_votes, budget)
     False
 
     >>> citizen_votes = [[3, 0, 0], [0, 0, 3], [3, 0, 0], [0, 0, 3], [3, 0, 0], [0, 0, 3], [3, 0, 0], [0, 0, 3], [3, 0, 0], [0, 0, 3]]
